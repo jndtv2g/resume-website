73e6aeef02a929c5425e4ab780b26be1a28164e4
jndtv2g <jvitug@ymail.com>
2026-01-12 13:43:51 +0800

    Updated tech stack

--- data/techStack.ts
+++ data/techStack.ts
@@ -3,7 +3,7 @@ export const techStack = {
   "Programming": ["JavaScript", "C#", "Java", "Swift"],
   "Automation Tools": ["Visual Studio", "Visual Studio Code", "Eclipse", "XCode", "MS SQL Server", "Oracle SQL Developer", "MySQL"],
   "Version Control & CI/CD": ["Github Desktop", "SourceTree", "Gitlab", "Bitbucket", "Azure DevOps"],
-  "Testing Methodologies": ["Functional Testing", "Automation Testing", "Regression Testing", "End-to-End Testing", "Exploratory Testing", "Sanity/Smoke Testing", "Accessibility Testing"],
+  "Testing Methodologies": ["Functional Testing", "Automation Testing", "Regression Testing", "End-to-End Testing", "Exploratory Testing", "Sanity/Smoke Testing", "Accessibility Testing", "Gherkin"],
   "Test Management": ["TestRail", "Zephyr", "BrowserStack", "Qase.io", "Azure DevOps"],
   "Defect Management": ["Jira", "ClickUp", "Azure DevOps"],
   "Debugging & Monitoring Tools": ["Fiddler", "Sumo Logic", "Screencopy", "Vysor", "Laravel Telescope", "Postman"],
